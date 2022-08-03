8fd90cd0c5898b3c54e0cd8d0accfee8b3b01504
pezzu <7243796+pezzu@users.noreply.github.com>
2022-08-02 21:43:52 -0400

    Generate HTML

--- src/gsheet-import.spec.ts
+++ src/gsheet-import.spec.ts
@@ -1,22 +1,76 @@
-import { createAuth, fetchGSheets } from "./gsheet-import";
+import { JSDOM } from 'jsdom';
+import { createAuth, fetchGSheets, toHtmlTable, renderHtml } from "./gsheet-import";
 
-describe("Imports data from Google Sheets", () => {
+describe("Imports data from Google Sheets to HTML", () => {
   /**
    * Google sample spreadsheet:
    * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
    */
-  it("should import data from Google Sheets", async () => {
+  it("Should fetch cells values from Google Sheets", async () => {
     const auth = createAuth("./credentials.json");
-    const values = await fetchGSheets(
+    const data = await fetchGSheets(
       auth,
       "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
       "Class Data!A2:E5"
     );
-    expect(values.values).toEqual([
+    expect(data.values).toEqual([
       ["Alexandra", "Female", "4. Senior", "CA", "English"],
       ["Andrew", "Male", "1. Freshman", "SD", "Math"],
       ["Anna", "Female", "1. Freshman", "NC", "English"],
       ["Becky",	"Female",	"2. Sophomore",	"SD",	"Art"],
     ]);
   });
+
+  it("Should two dimesional array to HTML table", () => {
+    const data = {
+      values: [
+        ["1-1", "1-2", "1-3", "1-4"],
+        ["2-1", "2-2", "2-3", "2-4"],
+        ["3-1", "3-2", "3-3", "3-4"],
+      ]
+    }
+
+    const html = toHtmlTable(data);
+    const dom = new JSDOM(html);
+
+    expect(dom.window.document.body.children.length).toEqual(1);
+
+    const table = dom.window.document.querySelector("table");
+    expect(table).toBeTruthy();
+    expect(table!.children[0].tagName).toEqual("TBODY");
+    expect(table!.children[0].children.length).toEqual(3);
+
+    Array.from(table!.children[0].children).forEach((row, i) => {
+      expect(row.children.length).toEqual(4);
+      Array.from(row.children).forEach((cell, j) => {
+        expect(cell.textContent).toEqual(`${i+1}-${j+1}`);
+      });
+    })
+  })
+
+  it("Should render mustache templates", () => {
+    const template = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>{{title}}</h1>
+        {{content}}
+      </body>
+      </html>
+    `;
+    const data = {
+      title: "Hello",
+      content: "<p>World</p>"
+    }
+    const html = renderHtml(template, data);
+    const dom = new JSDOM(html);
+    expect(dom.window.document.body.children.length).toEqual(2);
+    expect(dom.window.document.querySelector("h1")!.textContent).toEqual("Hello");
+    expect(dom.window.document.querySelector("p")!.textContent).toEqual("World");
+  })
 });
--- src/gsheet-import.ts
+++ src/gsheet-import.ts
@@ -3,7 +3,7 @@ import { google, sheets_v4, Auth } from "googleapis";
 const params = {
   keyFile: "./credentials.json",
   spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
-  range: "Class Data!A2:E",
+  range: "Class Data!A2:E3",
 };
 
 export function createAuth(
@@ -30,15 +30,45 @@ export async function fetchGSheets(
   return (await values).data;
 }
 
-function print(data: sheets_v4.Schema$ValueRange): void {
-  console.log(data.values);
+export function toHtmlTable(data: sheets_v4.Schema$ValueRange): string {
+  if (!data.values) return "";
+
+  const html = `<table>${data.values
+    .map((row) => `<tr>${row.map((cell) => `<td>${cell}</td>`).join("")}</tr>`)
+    .join("")}</table>`;
+  return html;
 }
 
+export function renderHtml(template: string, data: any): string {
+  return template.replace(/{{(.*?)}}/g, (match, key) => data[key]);
+}
+
+function print(data: any): void {
+  console.log(data);
+}
+
+const HTML_TEMPLATE = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>{{title}}</h1>
+  {{content}}
+</body>
+</html>
+`;
+
 if (require.main === module) {
-  function main(): void {
+  async function main(): Promise<void> {
     const auth = createAuth(params.keyFile);
-    const values = fetchGSheets(auth, params.spreadsheetId, params.range);
-    values.then(print);
+    const values = await fetchGSheets(auth, params.spreadsheetId, params.range);
+    const table = toHtmlTable(values);
+    const html = renderHtml(HTML_TEMPLATE, { content: table });
+    print(html);
   }
 
   main();
