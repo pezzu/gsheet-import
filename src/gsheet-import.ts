77516e99d6d5fbca11b41c6cc9eb5dd32f8452ff
pezzu <7243796+pezzu@users.noreply.github.com>
2022-08-07 16:57:52 -0400

    HTML template moved to separate file

--- src/gsheet-import.ts
+++ src/gsheet-import.ts
@@ -7,20 +7,6 @@ function print(data: any): void {
   console.log(data);
 }
 
-const HTML_TEMPLATE = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-{{content}}
-</body>
-</html>
-`;
-
 async function main(): Promise<void> {
   try {
     const params = JSON.parse(readFileSync("app-settings.json", "utf8"));
@@ -28,9 +14,11 @@ async function main(): Promise<void> {
     const cells = await fetch(params.credentials, params.sheet);
     if (cells.length > 0) {
       const table = toHtmlTable(cells);
-      const html = renderHtml(HTML_TEMPLATE, { content: table });
-      await sendMail(params.mail.transport, { ...params.mail.message, html });
-      print(`Mail sent to ${params.mail.message.to}`);
+      const template = readFileSync(params.mail.template.file, "utf8");
+      const html = renderHtml(template, { content: table });
+      // await sendMail(params.mail.transport, { ...params.mail.message, html });
+      // print(`Mail sent to ${params.mail.message.to}`);
+      print(html);
     }
   } catch (e) {
     console.error(e);
