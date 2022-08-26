75ef37abd517e910ed16ebfe526a8173cb1b6d9d
pezzu <7243796+pezzu@users.noreply.github.com>
2022-08-26 14:29:06 -0400

    Switched to swc/jest for performance of tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+  },
   testEnvironment: 'node',
 };
